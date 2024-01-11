f4b781202e8821752facfb64cb8e615e4066f787
kl63 <kl63@NJIT.edu>
2024-01-11 08:41:45 -0500

    minor changes

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -12,7 +12,7 @@ const Promotion = () => {
   })
 
   const targetDate = new Date()
-  targetDate.setDate(targetDate.getDate() + 3)
+  targetDate.setDate(targetDate.getDate() + 7)
 
   useEffect(() => {
     const timerInterval = setInterval(() => {
